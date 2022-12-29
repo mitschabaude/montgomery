bd38bcc0fe77f42af2af454b000ea3979261eb1d
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2022-12-29 10:53:50 +0100

    add some exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-export { msmAffine } from "./msm.js";
+import * as Field from "./finite-field.js";
+import * as Scalar from "./scalar-glv.js";
+import * as Curve from "./curve.js";
+export { Field, Scalar, Curve };
+export { msmAffine as msm } from "./msm.js";
