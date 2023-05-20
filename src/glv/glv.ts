e785021523009e23c6af3dce05a43867df57fb17
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2023-05-20 18:05:16 +0200

    comments

--- src/glv/glv.ts
+++ src/glv/glv.ts
@@ -8,6 +8,14 @@ export { egcdStopEarly };
  * Input: positive integers l, p
  * Output: matrix V = [[v00,v01],[v10,v11]] of field elements satisfying
  * (1, l)^T V = v0j + l*v1j = 0 (mod p) and |vij| ~ sqrt(p) for "random" l
+ *
+ * Fun fact: the determinant of V is either p or -p. Proof:
+ * - initially, det = r0 * t1 - r1 * t0 = p * 1 - l * 0 = p
+ * - in each iteration, det flips its sign:
+ * det' = r0' * t1' - r1' * t0' =
+ * (r1 * (t0 - quotient * t1)) - ((r0 - quotient * r1) * t1) =
+ * r1 * t0 - r1 * t1 * quotient - r0 * t1 + quotient * r1 * t1 =
+ * r1 * t0 - r0 * t1 = -det
  */
 function egcdStopEarly(l: bigint, p: bigint) {
   if (l > p) throw Error("a > p");
@@ -16,11 +24,6 @@ function egcdStopEarly(l: bigint, p: bigint) {
   let [t0, t1] = [0n, 1n];
   while (r1 * r1 > p) {
     let quotient = r0 / r1; // bigint division, cuts off remainder
-    // det' = r0' * t1' - r1' * t0' =
-    // (r1 * (t0 - quotient * t1)) - ((r0 - quotient * r1) * t1) =
-    // r1 * t0 - r1 * t1 * quotient - r0 * t1 + quotient * r1 * t1 =
-    // r1 * t0 - r0 * t1 = -det
-    // => det' = -det = +-p
     [r0, r1] = [r1, r0 - quotient * r1];
     [s0, s1] = [s1, s0 - quotient * s1];
     [t0, t1] = [t1, t0 - quotient * t1];
