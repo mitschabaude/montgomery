6c1c73ac28e0f327c19185a0070c604d9da55b08
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2023-08-07 22:21:09 +0200

    start to structure as loop over limbs

--- src/inverse/faster-inverse.ts
+++ src/inverse/faster-inverse.ts
@@ -1,71 +1,65 @@
 import { Field, Random } from "../concrete/pasta.js";
 import { mod, modInverse } from "../field-util.js";
-import { assert } from "../util.js";
+import { assert, log2 } from "../util.js";
 
 const { p } = Field;
+let b = Field.bitLength;
 
 const w = 29n;
+const n = Math.ceil(b / Number(w));
 
-let x = Random.randomField();
-// let x =
-//   21055668057744206722049915034365825947141946607484548719881883519400681427846n;
+// let x = Random.randomField();
+let x =
+  21055668057744206722049915034365825947141946607484548719881883519400681427846n;
 let z = modInverse(x, p);
 
 assert(mod(z * x, p) === 1n);
 
-let [r, k] = almostInverse(x, p);
+let [r, k] = almostInverse(x, p, w, n);
 
-let b = Field.bitLength;
-assert(k + 1n >= b && k < 2 * b);
-assert(r < p, "r < p");
+// assert(k + 1n >= b && k < 2 * b, "k bounds");
+// assert(r < p, "r < p");
 
 assert(mod(x * r - (1n << k), p) === 0n, "almost inverse");
 
-function almostInverse(a: bigint, p: bigint) {
+function almostInverse(a: bigint, p: bigint, w: bigint, n: number) {
   let u = -p;
   let v = a;
   let r = 0n;
   let s = 1n;
   let k = 0n;
 
-  while (true) {
-    if ((u & (1n << k)) === 0n) {
-      console.log("reduce u");
-      v <<= 1n;
-      s <<= 1n;
-    } else if ((v & (1n << k)) === 0n) {
-      console.log("reduce v");
-      u <<= 1n;
-      r <<= 1n;
-    } else {
-      let m = u + v;
-      console.log({ u, v, m, k });
-      if (m === 0n) break;
-      if (m < 0n) {
-        u = m;
-        r = r + s;
+  for (let i = 0; i < 2 * n; i++) {
+    for (let j = 0; j < w; j++) {
+      if ((u & (1n << k)) === 0n) {
+        console.log("reduce u");
         v <<= 1n;
         s <<= 1n;
-      } else {
-        v = m;
-        s = r + s;
+      } else if ((v & (1n << k)) === 0n) {
+        console.log("reduce v");
         u <<= 1n;
         r <<= 1n;
+      } else {
+        let m = u + v;
+        console.log({ u, v, m, k });
+        if (m <= 0n) {
+          u = m;
+          r = r + s;
+          v <<= 1n;
+          s <<= 1n;
+        } else {
+          v = m;
+          s = r + s;
+          u <<= 1n;
+          r <<= 1n;
+        }
       }
+      k++;
+      assert(v * r - u * s === 2n ** k * p, "linear combination");
+      assert(mod(a * r - u, p) === 0n, "mod p, r");
+      assert(mod(a * s - v, p) === 0n, "mod p, s");
     }
-    k++;
-    assert(v * r - u * s === 2n ** k * p);
-    assert(mod(a * r - u, p) === 0n);
-    assert(mod(a * s - v, p) === 0n);
+    if (u === 0n) break;
   }
   return [s, k];
-}
-
-function makeOdd(u: bigint, s: bigint, k: bigint) {
-  while ((u & 1n) === 0n) {
-    u >>= 1n;
-    s <<= 1n;
-    k++;
-  }
-  return [u, s, k];
 }
