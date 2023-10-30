3dba656b371ec1ff8e59ed6532a89d2fcb4dbeeb
Gregor <gregor.mitscha-baude@gmx.at>
2023-10-30 09:52:13 +0100

    start instantiating and testing 277. we're missing subgroup handling

---
+++ scripts/test-bls12377-msm.ts
@@ -0,0 +1,54 @@
+import "../src/extra/fix-webcrypto.js";
+import { tic, toc } from "../src/extra/tictoc.js";
+import {
+  msm,
+  msmUtil,
+  Field,
+  CurveAffine,
+  Random,
+  Scalar,
+  Bigint,
+} from "../src/concrete/bls12-377.js";
+import { bigintScalarsToMemory } from "../src/msm.js";
+import { checkOnCurve, msmDumbAffine } from "../src/extra/dumb-curve-affine.js";
+import assert from "node:assert/strict";
+
+let n = Number(process.argv[2] ?? 8);
+let N = 1 << n;
+console.log(`running msm with 2^${n} = ${2 ** n} inputs`);
+
+tic("random points");
+let points = Field.getZeroPointers(N, CurveAffine.sizeAffine);
+let scratch = Field.getPointers(20);
+CurveAffine.randomPoints(scratch, points);
+
+let scalars = Random.randomScalars(N);
+let scalarPtr = bigintScalarsToMemory(Scalar, scalars);
+toc();
+
+tic("convert points to bigint & check");
+let pointsBigint = points.map((gPtr) => {
+  let g = CurveAffine.toBigint(gPtr);
+  assert(checkOnCurve(g, Field.p, CurveAffine.b), "point on curve");
+  return g;
+});
+toc();
+
+tic("msm (core)");
+let s0 = msm(scalarPtr, points[0], N);
+let s = msmUtil.toAffineOutputBigint(scratch, s0);
+toc();
+
+tic("msm (bigint)");
+let s1 = Bigint.msm(scalars, pointsBigint);
+toc();
+
+assert.deepEqual(s, s1, "consistent with bigint version");
+
+if (n < 12) {
+  tic("msm (simple, slow bigint impl)");
+  let sBigint = msmDumbAffine(scalars, pointsBigint, Scalar, Field);
+  toc();
+  assert.deepEqual(s, sBigint, "consistent results");
+  console.log("results are consistent!");
+}
---
+++ src/concrete/bls12-377.ts
@@ -0,0 +1,42 @@
+import type * as W from "wasmati";
+import { randomGenerators } from "../field-util.js";
+import { p, q, b, beta, lambda } from "./bls12-377.params.js";
+import { createMsmField } from "../field-msm.js";
+import { createGeneralGlvScalar } from "../scalar-glv.js";
+import { createCurveAffine } from "../curve-affine.js";
+import { createCurveProjective } from "../curve-projective.js";
+import { createMsm } from "../msm.js";
+import { createBigintApi } from "../bigint.js";
+
+export { Bigint, Field, Scalar, CurveAffine, CurveProjective, Random };
+export { msm, msmUnsafe, msmUtil };
+
+const Field = await createMsmField(p, beta, 29);
+const Scalar = await createGeneralGlvScalar(q, lambda, 29);
+const CurveAffine = createCurveAffine(Field, b);
+const CurveProjective = createCurveProjective(Field);
+
+const { msm, msmUnsafe, msmBigint, ...msmUtil } = createMsm({
+  Field,
+  Scalar,
+  CurveAffine,
+  CurveProjective,
+});
+
+const { randomField: randomScalar, randomFields: randomScalars } =
+  randomGenerators(q);
+
+const Random = { ...randomGenerators(p), randomScalar, randomScalars };
+
+const Bigint_ = createBigintApi({
+  Field,
+  Scalar,
+  CurveAffine,
+  CurveProjective,
+});
+const Bigint = {
+  ...Bigint_,
+  msm: msmBigint,
+  randomFields: Random.randomFields,
+  randomScalars,
+};
