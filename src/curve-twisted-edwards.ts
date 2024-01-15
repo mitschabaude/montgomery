e59d1a6ad86377b2a3d4889a118e3418f5f78447
Gregor <gregor.mitscha-baude@gmx.at>
2024-01-15 17:33:01 +0100

    remove invalid code

--- src/curve-twisted-edwards.ts
+++ src/curve-twisted-edwards.ts
@@ -33,17 +33,7 @@ function createCurveTwistedEdwards(
   // convert the cofactor to bits
   let cofactorBits = bigintToBits(cofactor);
 
-  const {
-    sizeField,
-    square,
-    multiply,
-    add,
-    subtract,
-    copy,
-    isEqual,
-    memoryBytes,
-    p,
-  } = Field;
+  const { sizeField, square, multiply, add, copy, memoryBytes, p } = Field;
 
   // memory layout: x | y | z | t | isNonZero
   let size = 4 * sizeField + 4;
@@ -83,64 +73,9 @@ function createCurveTwistedEdwards(
     }
     if (isZero(P2)) return;
     setNonZero(P1);
-    let [X1, Y1, Z1] = coords(P1);
-    let [X2, Y2, Z2] = coords(P2);
-    let [Y2Z1, Y1Z2, X2Z1, X1Z2, Z1Z2, u, uu, v, vv, vvv, R] = scratch;
-    // http://www.hyperelliptic.org/EFD/g1p/auto-shortw-projective.html#addition-add-1998-cmo-2
-    // Y1Z2 = Y1*Z2
-    multiply(Y1Z2, Y1, Z2);
-    // Y2Z1 = Y2*Z1
-    multiply(Y2Z1, Y2, Z1);
-    // X1Z2 = X1*Z2
-    multiply(X1Z2, X1, Z2);
-    // X2Z1 = X2*Z1
-    multiply(X2Z1, X2, Z1);
-
-    // double if the points are equal
-    // x1*z2 = x2*z1 and y1*z2 = y2*z1
-    // <==>  x1/z1 = x2/z2 and y1/z1 = y2/z2
-    Field.reduce(X1Z2);
-    Field.reduce(X2Z1);
-    if (isEqual(X1Z2, X2Z1)) {
-      Field.reduce(Y1Z2);
-      Field.reduce(Y2Z1);
-      if (isEqual(Y1Z2, Y2Z1)) {
-        doubleInPlace(scratch, P1);
-        return;
-      } else {
-        setZero(P1);
-        return;
-      }
-    }
-    // Z1Z2 = Z1*Z2
-    multiply(Z1Z2, Z1, Z2);
-    // u = Y2Z1-Y1Z2
-    subtract(u, Y2Z1, Y1Z2);
-    // uu = u^2
-    square(uu, u);
-    // v = X2Z1-X1Z2
-    subtract(v, X2Z1, X1Z2);
-    // vv = v^2
-    square(vv, v);
-    // vvv = v*vv
-    multiply(vvv, v, vv);
-    // R = vv*X1Z2
-    multiply(R, vv, X1Z2);
-    // A = uu*Z1Z2-vvv-2*R
-    let A = uu;
-    multiply(A, uu, Z1Z2);
-    subtract(A, A, vvv);
-    subtract(A, A, R);
-    subtract(A, A, R);
-    // X3 = v*A
-    multiply(X1, v, A);
-    // Y3 = u*(R-A)-vvv*Y1Z2
-    subtract(R, R, A);
-    multiply(Y1, u, R);
-    multiply(Y1Z2, vvv, Y1Z2);
-    subtract(Y1, Y1, Y1Z2);
-    // Z3 = vvv*Z1Z2
-    multiply(Z1, vvv, Z1Z2);
+    let [X1, Y1, Z1, T1] = coords(P1);
+    let [X2, Y2, Z2, T2] = coords(P2);
+    assert(false, "TODO");
   }
 
   /**
@@ -151,40 +86,8 @@ function createCurveTwistedEdwards(
    */
   function doubleInPlace(scratch: number[], P: number) {
     if (isZero(P)) return;
-    let [X1, Y1, Z1] = coords(P);
-    let [tmp, w, s, ss, sss, Rx2, Bx4, h] = scratch;
-    // http://www.hyperelliptic.org/EFD/g1p/auto-shortw-projective.html#doubling-dbl-1998-cmo-2
-    // w = 3*X1^2
-    square(w, X1);
-    add(tmp, w, w); // TODO efficient doubling
-    add(w, tmp, w);
-    // s = Y1*Z1
-    multiply(s, Y1, Z1);
-    // ss = s^2
-    square(ss, s);
-    // sss = s*ss
-    multiply(sss, ss, s);
-    // R = Y1*s, Rx2 = R + R
-    multiply(Rx2, Y1, s);
-    add(Rx2, Rx2, Rx2);
-    // 2*B (= X1*R), Bx4 = 2*B+2*B
-    multiply(Bx4, X1, Rx2);
-    add(Bx4, Bx4, Bx4);
-    // h = w^2-8*B = w^2 - Bx4 - Bx4
-    square(h, w);
-    subtract(h, h, Bx4); // TODO efficient doubling
-    subtract(h, h, Bx4);
-    // X3 = 2*h*s
-    multiply(X1, h, s);
-    add(X1, X1, X1);
-    // Y3 = w*(4*B-h)-8*R^2 = (Bx4 - h)*w - (Rx2^2 + Rx2^2)
-    subtract(Y1, Bx4, h);
-    multiply(Y1, Y1, w);
-    square(tmp, Rx2);
-    add(tmp, tmp, tmp); // TODO efficient doubling
-    subtract(Y1, Y1, tmp);
-    // Z3 = 8*sss
-    multiply(Z1, sss, Field.constants.mg8); // TODO efficient doubling
+    let [X1, Y1, Z1, T1] = coords(P);
+    assert(false, "TODO");
   }
 
   function scale(
@@ -203,7 +106,7 @@ function createCurveTwistedEdwards(
   }
 
   function toSubgroupInPlace(
-    [tmp, _tmpy, _tmpz, _tmpInf, ...scratch]: number[],
+    [tmp, _tmpy, _tmpz, _tmpt, _tmpInf, ...scratch]: number[],
     point: number
   ) {
     if (cofactor === 1n) return;
@@ -232,22 +135,14 @@ function createCurveTwistedEdwards(
     for (let i = 0; i < n; i++) {
       let x0 = xs[i];
       let x = points[i];
-      let y = x + sizeField;
 
       // copy x into memory
       Field.writeBigint(x, x0);
       Field.toMontgomery(x);
 
       while (true) {
-        // compute sqrt(x^3 + b), store in y
-        square(y, x);
-        multiply(y, y, x);
-        add(y, y, dPtr);
-        let isSquare = Field.sqrt(scratch, y, y);
-
-        // if we didn't find a square root, try again with x+1
-        if (isSquare) break;
-        add(x, x, Field.constants.mg1);
+        // TODO
+        assert(false, "TODO");
       }
       setNonZero(x);
     }
@@ -262,14 +157,8 @@ function createCurveTwistedEdwards(
 
   // note: this fails on zero
   function assertOnCurve([y2, y2_]: number[], p: number) {
-    let [x, y] = coords(p);
-    square(y2, x);
-    multiply(y2, y2, x);
-    add(y2, y2, dPtr);
-    Field.reduce(y2);
-    Field.square(y2_, y);
-    Field.reduce(y2_);
-    assert(Field.isEqual(y2_, y2) === 1, "point on curve");
+    let [X, Y, Z, T] = coords(p);
+    assert(false, "TODO");
   }
 
   function toBigint(point: number): BigintPoint {
