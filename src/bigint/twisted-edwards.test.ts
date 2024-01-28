5a51fff3b798174b7225e5ede96069bc8ad17a09
Gregor <gregor.mitscha-baude@gmx.at>
2024-01-28 21:27:35 +0100

    make twisted edwards test more general

--- src/bigint/twisted-edwards.test.ts
+++ src/bigint/twisted-edwards.test.ts
@@ -1,121 +1,157 @@
 import { Random, test } from "../testing/property.js";
-import { createCurveTwistedEdwards } from "./twisted-edwards.js";
-import { curveParams } from "../concrete/ed-on-bls12-377.params.js";
+import {
+  createCurveTwistedEdwards,
+  BigintPoint as TePoint,
+} from "./twisted-edwards.js";
+import { curveParams as teParams } from "../concrete/ed-on-bls12-377.params.js";
 import { assert } from "../util.js";
 
-let Curve = createCurveTwistedEdwards(curveParams);
+let testInputs: TestInput<any>[] = [
+  {
+    // twisted edwards curve
+    label: "ed-on-bls12-377",
+    Curve: createCurveTwistedEdwards(teParams),
+    randomShape: (f) => Random.record({ X: f, Y: f, Z: f, T: f }),
+  } satisfies TestInput<TePoint>,
+];
+
+for (let input of testInputs) testCurve(input);
 
 // TODO use the same tests for other curve implementations as well, like affine and projective Weierstrass
 
-let scalar = Random.field(Curve.order);
-let point = Random(Curve.random);
+type TestInput<Point> = {
+  label: string;
+  Curve: CurveInput<Point>;
+  randomShape: (field: Random<bigint>) => Random<Point>;
+};
 
-const uniformField = Random.uniformField(Curve.modulus);
-const notAPoint = Random.record({
-  X: uniformField,
-  Y: uniformField,
-  Z: uniformField,
-  T: uniformField,
-});
+function testCurve<Point>({ label, Curve, randomShape }: TestInput<Point>) {
+  let scalar = Random.field(Curve.order);
+  let point = Random(Curve.random);
 
-test.verbose(
-  "ed-on-bls12-377",
-  point,
-  point,
-  point,
-  notAPoint,
-  scalar,
-  scalar,
-  (P, Q, R, noP, s, t) => {
-    // random point is as expected
-    assert(Curve.isOnCurve(P), "point is on curve");
-    assert(Curve.isInSubgroup(P), "order * P = ∞");
+  const uniformField = Random.uniformField(Curve.modulus);
+  const notAPoint = randomShape(uniformField);
 
-    // random points are not equal, and not zero
-    assert(!Curve.isEqual(P, Q), "random points are not equal");
-    assert(!Curve.isZero(P), "random points are not zero");
+  test.verbose(
+    label,
+    point,
+    point,
+    point,
+    notAPoint,
+    scalar,
+    scalar,
+    (P, Q, R, noP, s, t) => {
+      // random point is as expected
+      assert(Curve.isOnCurve(P), "point is on curve");
+      assert(Curve.isInSubgroup(P), "order * P = ∞");
 
-    // isOnCurve detects points not on the curve
-    assert(!Curve.isOnCurve(noP), "isOnCurve");
+      // random points are not equal, and not zero
+      assert(!Curve.isEqual(P, Q), "random points are not equal");
+      assert(!Curve.isZero(P), "random points are not zero");
 
-    // a point equals itself, and zero equals zero
-    assert(Curve.isEqual(P, P), "P = P");
-    assert(Curve.isZero(Curve.zero), "zero is zero");
+      // isOnCurve detects points not on the curve
+      assert(!Curve.isOnCurve(noP), "isOnCurve");
 
-    // addition maps to the curve
-    assert(Curve.isOnCurve(Curve.add(P, Q)), "addition maps to the curve");
+      // a point equals itself, and zero equals zero
+      assert(Curve.isEqual(P, P), "P = P");
+      assert(Curve.isZero(Curve.zero), "zero is zero");
 
-    // negation maps to the curve
-    assert(Curve.isOnCurve(Curve.negate(P)), "negation maps to the curve");
+      // addition maps to the curve
+      assert(Curve.isOnCurve(Curve.add(P, Q)), "addition maps to the curve");
 
-    // addition is commutative
-    assert(
-      Curve.isEqual(Curve.add(P, Q), Curve.add(Q, P)),
-      "addition is commutative"
-    );
+      // negation maps to the curve
+      assert(Curve.isOnCurve(Curve.negate(P)), "negation maps to the curve");
 
-    // addition is associative
-    assert(
-      Curve.isEqual(
-        Curve.add(P, Curve.add(Q, R)),
-        Curve.add(Curve.add(P, Q), R)
-      ),
-      "addition is associative"
-    );
+      // addition is commutative
+      assert(
+        Curve.isEqual(Curve.add(P, Q), Curve.add(Q, P)),
+        "addition is commutative"
+      );
 
-    // addition is distributive
-    assert(
-      Curve.isEqual(
-        Curve.scale(s, Curve.add(P, Q)),
-        Curve.add(Curve.scale(s, P), Curve.scale(s, Q))
-      ),
-      "addition is distributive"
-    );
+      // addition is associative
+      assert(
+        Curve.isEqual(
+          Curve.add(P, Curve.add(Q, R)),
+          Curve.add(Curve.add(P, Q), R)
+        ),
+        "addition is associative"
+      );
 
-    // doubling is scaling by 2
-    assert(
-      Curve.isEqual(Curve.double(P), Curve.scale(2n, P)),
-      "double = scale 2"
-    );
+      // addition is distributive
+      assert(
+        Curve.isEqual(
+          Curve.scale(s, Curve.add(P, Q)),
+          Curve.add(Curve.scale(s, P), Curve.scale(s, Q))
+        ),
+        "addition is distributive"
+      );
 
-    // adding the negation is zero
-    assert(
-      Curve.isEqual(Curve.add(P, Curve.negate(P)), Curve.zero),
-      "P + -P = 0"
-    );
+      // doubling is scaling by 2
+      assert(
+        Curve.isEqual(Curve.double(P), Curve.scale(2n, P)),
+        "double = scale 2"
+      );
 
-    // negating twice is the identity
-    assert(Curve.isEqual(Curve.negate(Curve.negate(P)), P), "-(-P) = P");
+      // adding the negation is zero
+      assert(
+        Curve.isEqual(Curve.add(P, Curve.negate(P)), Curve.zero),
+        "P + -P = 0"
+      );
 
-    // scaling by order-1 is negation
-    assert(
-      Curve.isEqual(Curve.scale(Curve.order - 1n, P), Curve.negate(P)),
-      "scaling by order-1 is negation"
-    );
+      // negating twice is the identity
+      assert(Curve.isEqual(Curve.negate(Curve.negate(P)), P), "-(-P) = P");
 
-    // zero is the identity
-    assert(Curve.isEqual(Curve.add(P, Curve.zero), P), "P + 0 = P");
-    assert(Curve.isEqual(Curve.add(Curve.zero, P), P), "0 + P = P");
+      // scaling by order-1 is negation
+      assert(
+        Curve.isEqual(Curve.scale(Curve.order - 1n, P), Curve.negate(P)),
+        "scaling by order-1 is negation"
+      );
 
-    // scaling maps to the curve
-    assert(Curve.isOnCurve(Curve.scale(s, P)), "scaling maps to the curve");
+      // zero is the identity
+      assert(Curve.isEqual(Curve.add(P, Curve.zero), P), "P + 0 = P");
+      assert(Curve.isEqual(Curve.add(Curve.zero, P), P), "0 + P = P");
 
-    // scaling by a non-zero scalar yields a non-zero point
-    assert(
-      s === 0n || !Curve.isZero(Curve.scale(s, P)),
-      "scaling is injective"
-    );
+      // scaling maps to the curve
+      assert(Curve.isOnCurve(Curve.scale(s, P)), "scaling maps to the curve");
 
-    // scaling by two scalars is scaling by the product
-    assert(
-      Curve.isEqual(
-        Curve.scale(s, Curve.scale(t, P)),
-        Curve.scale(Curve.Scalar.multiply(s, t), P)
-      ),
-      "scaling twice"
-    );
+      // scaling by a non-zero scalar yields a non-zero point
+      assert(
+        s === 0n || !Curve.isZero(Curve.scale(s, P)),
+        "scaling is injective"
+      );
 
-    // the generator is on the curve
-    assert(Curve.isOnCurve(Curve.one), "generator is on the curve");
-  }
-);
+      // scaling by two scalars is scaling by the product
+      assert(
+        Curve.isEqual(
+          Curve.scale(s, Curve.scale(t, P)),
+          Curve.scale(Curve.Scalar.multiply(s, t), P)
+        ),
+        "scaling twice"
+      );
+
+      // the generator is on the curve
+      assert(Curve.isOnCurve(Curve.one), "generator is on the curve");
+    }
+  );
+}
+
+type CurveInput<Point> = {
+  modulus: bigint;
+  order: bigint;
+
+  zero: Point;
+  one: Point;
+  random: () => Point;
+  add: (P: Point, Q: Point) => Point;
+  negate: (P: Point) => Point;
+  double: (P: Point) => Point;
+  scale: (s: bigint, P: Point) => Point;
+  isEqual: (P: Point, Q: Point) => boolean;
+  isZero: (P: Point) => boolean;
+  isOnCurve: (P: Point) => boolean;
+  isInSubgroup: (P: Point) => boolean;
+
+  Scalar: {
+    multiply: (s: bigint, t: bigint) => bigint;
+  };
+};
