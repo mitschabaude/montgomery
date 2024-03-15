c8f60b78c8b8b2e6183aec7215b79062cf6263a8
Gregor <gregor.mitscha-baude@gmx.at>
2024-03-15 19:45:31 +0100

    better work distribution algorithm

--- src/msm-common.ts
+++ src/msm-common.ts
@@ -1,3 +1,4 @@
+import { assert } from "./util.js";
 import type { MemorySection } from "./wasm/memory-helpers.js";
 
 export { windowSize, splitBuckets, Chunk, createLog };
@@ -61,6 +62,13 @@ type Chunk = {
   length: number;
 };
 
+/**
+ * Split buckets among threads
+ *
+ * Note: this takes into account
+ * - bucket sizes not being distributed evenly in the final partition
+ * - peculiarity in the bucket distribution when the signed digits algorithm is used
+ */
 function splitBuckets(
   {
     Field,
@@ -78,55 +86,80 @@ function splitBuckets(
   THREADS: number
 ) {
   let { b, c, K, L } = params;
+  // Ll = number of non-empty buckets in final partition
   let overlapBits = b % c;
-  let Llast = 2 ** overlapBits;
-  // log("expected points per bucket", {
-  //   default: (4 * N) / L,
-  //   last: (4 * N) / Llast,
-  //   overlapBits,
-  //   scalarBitlength: b,
-  // });
+  let Ll = overlapBits === 0 ? 1 : 1 << overlapBits; // the 1 is because overflow of carries in the signed digit algorithm
+  let wl = overlapBits === 0 ? L / Ll / 2 ** 5 : L / Ll; // reduced weight in the overflow case
 
-  let totalWork = K * L;
-  let nt = Math.ceil(totalWork / THREADS);
+  let totalWork = (K - 1) * L + Ll * wl; // = K*L except in the overflow case
+  let workPerThread = Math.ceil(totalWork / THREADS);
 
   let chunksPerThread: Chunk[][] = [];
   let nChunksPerPartition: number[] = Array(K);
 
   let thread = 0;
-  let remainingWork = nt;
+  let remainingForCurrentThread = workPerThread;
 
-  for (let k = 0; k < K; k++) {
+  for (let k = 0; k < K - 1; k++) {
     let j = 0;
-    let remainingL = L;
+    let remainingInThisPartition = L;
     let lstart = 1;
-    while (remainingL > 0) {
-      let length = Math.min(remainingL, remainingWork);
+    while (remainingInThisPartition > 0) {
+      let length = Math.min(
+        remainingInThisPartition,
+        remainingForCurrentThread
+      );
       chunksPerThread[thread] ??= [];
       chunksPerThread[thread].push({ k, j, lstart, length });
       j++;
-      remainingL -= length;
+      remainingInThisPartition -= length;
+      remainingForCurrentThread -= length;
       lstart += length;
-      remainingWork -= length;
-      if (remainingWork === 0) {
+      if (remainingForCurrentThread <= 0) {
         thread++;
-        remainingWork = nt;
+        remainingForCurrentThread = workPerThread;
       }
     }
     nChunksPerPartition[k] = j;
   }
+  {
+    let k = K - 1;
+    let j = 0;
+    let remainingInThisPartition = Ll * wl;
+    let lstart = 1;
+    while (remainingInThisPartition > 0) {
+      let length = Math.ceil(
+        Math.min(remainingInThisPartition, remainingForCurrentThread) / wl
+      );
+      chunksPerThread[thread] ??= [];
+      chunksPerThread[thread].push({ k, j, lstart, length });
+      j++;
+      remainingInThisPartition -= wl * length;
+      remainingForCurrentThread -= wl * length;
+      lstart += length;
+      if (remainingInThisPartition <= 0) {
+        // last chunk! push the remaining, empty buckets to this chunk as well?
+        assert(lstart > Ll);
+      }
+      if (remainingForCurrentThread <= 0) {
+        thread++;
+        remainingForCurrentThread = workPerThread;
+      }
+    }
+    assert(thread <= THREADS);
 
-  // allocate space for different threads' contribution to each partitions
+    nChunksPerPartition[k] = j;
+  }
+
+  // allocate space for different threads' contribution to each partition
   // K x (#chunks in this partition)
   let chunkSumsPerPartition: Uint32Array[] = Array(K);
   for (let k = 0; k < K; k++) {
     let nChunks = nChunksPerPartition[k];
-    chunkSumsPerPartition[k] = new Uint32Array(nChunks);
-    // note: each thread must compute this independently in the same way
-    let chunkPtrs = Field.global.getPointers(nChunks, Curve.size);
-    for (let j = 0; j < nChunks; j++) {
-      chunkSumsPerPartition[k][j] = chunkPtrs[j];
-    }
+    // note: each thread must compute these pointers independently in the same way
+    chunkSumsPerPartition[k] = Uint32Array.from(
+      Field.global.getPointers(nChunks, Curve.size)
+    );
   }
 
   return { chunksPerThread, chunkSumsPerPartition };
