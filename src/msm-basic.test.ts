d693a1872050d13be7a06e23aba7fb3853b13f58
Gregor <gregor.mitscha-baude@gmx.at>
2024-03-15 15:14:00 +0100

    refactor test

--- src/msm-basic.test.ts
+++ src/msm-basic.test.ts
@@ -1,50 +1,36 @@
-import { msmBasic } from "./msm-basic.js";
 import { msm as msmBigint } from "./bigint/msm.js";
 import { curveParams } from "./concrete/ed-on-bls12-377.params.js";
-import { createMsmField } from "./field-msm.js";
-import { createCurveTwistedEdwards } from "./curve-twisted-edwards.js";
-import { createCurveTwistedEdwards as createCurveBigint } from "./bigint/twisted-edwards.js";
-import { createScalar } from "./scalar-simple.js";
-import { createRandomPointsFast, createRandomScalars } from "./curve-random.js";
 import { tic, toc } from "./testing/tictoc.js";
 import { assert } from "./util.js";
+import { create } from "./module-twisted-edwards.js";
 
-const Field = await createMsmField({ p: curveParams.modulus, w: 29, beta: 1n });
-const Scalar = await createScalar({ q: curveParams.order, w: 29 });
-const Curve = createCurveTwistedEdwards(Field, curveParams);
-const BigintCurve = createCurveBigint(curveParams);
-
-let randomPoints = createRandomPointsFast({
-  Field,
-  Affine: Curve,
-  Projective: Curve,
-});
-const randomScalars = createRandomScalars({ Scalar });
+const Curve = await create(curveParams);
 
 const n = 10;
 const N = 1 << n;
 
 // create random input points & scalars
 tic("random inputs");
-let points = await randomPoints(N);
-let scalars = await randomScalars(N);
+let points = await Curve.Parallel.randomPointsFast(N);
+let scalars = await Curve.Parallel.randomScalars(N);
 toc();
 
 // run msm
 tic("msm");
-let result = await msmBasic({ Field, Scalar, Curve }, scalars, points, N);
-let s0 = Curve.toBigint(result);
+let result = await Curve.Parallel.msm(scalars, points, N);
+let s0 = Curve.Curve.toBigint(result);
 toc();
 
 // convert points and scalars to bigints
 tic("inputs to bigints");
-let pointsBigint = points.map(Curve.toBigint);
-let scalarsBigint = scalars.map(Scalar.toBigint);
+let pointsBigint = points.map(Curve.Curve.toBigint);
+let scalarsBigint = scalars.map(Curve.Scalar.toBigint);
 toc();
 
 // run bigint msm
 tic("bigint msm");
-let s1 = msmBigint(BigintCurve, scalarsBigint, pointsBigint);
+let s1 = msmBigint(Curve.Bigint, scalarsBigint, pointsBigint);
 toc();
 
-assert(BigintCurve.isEqual(s0, s1), "msm result mismatch");
+assert(Curve.Bigint.isEqual(s0, s1), "msm result mismatch");
+assert(Curve.Bigint.isZero(s1) === false, "msm result is zero");
