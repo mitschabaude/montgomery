a41604795a665b4c61da12690090f6e5062ff02d
Gregor <gregor.mitscha-baude@gmx.at>
2024-03-15 11:26:03 +0100

    basic msm that works

--- src/msm-basic.test.ts
+++ src/msm-basic.test.ts
@@ -1,13 +1,18 @@
 import { msmBasic } from "./msm-basic.js";
+import { msm as msmBigint } from "./bigint/msm.js";
 import { curveParams } from "./concrete/ed-on-bls12-377.params.js";
 import { createMsmField } from "./field-msm.js";
 import { createCurveTwistedEdwards } from "./curve-twisted-edwards.js";
+import { createCurveTwistedEdwards as createCurveBigint } from "./bigint/twisted-edwards.js";
 import { createScalar } from "./scalar-simple.js";
 import { createRandomPointsFast, createRandomScalars } from "./curve-random.js";
+import { tic, toc } from "./testing/tictoc.js";
+import { assert } from "./util.js";
 
 const Field = await createMsmField({ p: curveParams.modulus, w: 29, beta: 1n });
 const Scalar = await createScalar({ q: curveParams.order, w: 29 });
 const Curve = createCurveTwistedEdwards(Field, curveParams);
+const BigintCurve = createCurveBigint(curveParams);
 
 let randomPoints = createRandomPointsFast({
   Field,
@@ -20,7 +25,26 @@ const n = 10;
 const N = 1 << n;
 
 // create random input points & scalars
-let [points] = await randomPoints(N);
-let [scalars] = await randomScalars(N);
+tic("random inputs");
+let points = await randomPoints(N);
+let scalars = await randomScalars(N);
+toc();
 
-await msmBasic({ Field, Scalar, Curve }, scalars, points, N);
+// run msm
+tic("msm");
+let result = await msmBasic({ Field, Scalar, Curve }, scalars, points, N);
+let s0 = Curve.toBigint(result);
+toc();
+
+// convert points and scalars to bigints
+tic("inputs to bigints");
+let pointsBigint = points.map(Curve.toBigint);
+let scalarsBigint = scalars.map(Scalar.toBigint);
+toc();
+
+// run bigint msm
+tic("bigint msm");
+let s1 = msmBigint(BigintCurve, scalarsBigint, pointsBigint);
+toc();
+
+assert(BigintCurve.isEqual(s0, s1), "msm result mismatch");
--- src/msm-basic.ts
+++ src/msm-basic.ts
@@ -23,11 +23,56 @@ type MsmInputCurve = {
 
 async function msmBasic(
   { Field, Scalar, Curve }: MsmInputCurve,
-  inputScalarPtr: number,
-  inputPointPtr: number,
+  scalars: number[],
+  points: number[],
   N: number
 ) {
   let b = Scalar.sizeInBits;
   let n = log2(N);
   let c = Math.max(n - 1, 1); // window size
+  let K = Math.ceil(b / c);
+  let L = 1 << c;
+
+  let { addAssign, doubleInPlace, setZero } = Curve;
+  let result = Field.global.getPointer(Curve.size);
+
+  using _g = Field.global.atCurrentOffset;
+  using _l = Field.local.atCurrentOffset;
+  let scratch = Field.local.getPointers(40);
+  let partitionSums = Uint32Array.from(Field.global.getPointers(K, Curve.size));
+
+  for (let k = 0; k < K; k++) {
+    using _l = Field.local.atCurrentOffset;
+    let runningSum = Field.local.getPointer(Curve.size);
+    let triangleSum = Field.local.getPointer(Curve.size);
+    let buckets = Uint32Array.from(Field.local.getPointers(L - 1, Curve.size));
+    for (let l = 0; l < L - 1; l++) setZero(buckets[l]);
+
+    // accumulation
+    for (let i = 0; i < N; i++) {
+      let l = Scalar.extractBitSlice(scalars[i], k * c, c);
+      if (l === 0) continue;
+      addAssign(scratch, buckets[l - 1], points[i]);
+    }
+
+    // triangle sum
+    setZero(runningSum);
+    setZero(triangleSum);
+    for (let l = L - 2; l >= 0; l--) {
+      addAssign(scratch, runningSum, buckets[l]);
+      addAssign(scratch, triangleSum, runningSum);
+    }
+    Curve.copy(partitionSums[k], triangleSum);
+  }
+
+  // final summation
+  Curve.copy(result, partitionSums[K - 1]);
+  for (let k = K - 2; k >= 0; k--) {
+    for (let i = 0; i < c; i++) {
+      doubleInPlace(scratch, result);
+    }
+    addAssign(scratch, result, partitionSums[k]);
+  }
+
+  return result;
 }
