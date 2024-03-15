0317022f3c65641deebca4ee416d2f9112afcf85
Gregor <gregor.mitscha-baude@gmx.at>
2024-03-15 15:23:52 +0100

    switch on parallelism - it works!

--- src/msm-basic.test.ts
+++ src/msm-basic.test.ts
@@ -2,12 +2,14 @@ import { msm as msmBigint } from "./bigint/msm.js";
 import { curveParams } from "./concrete/ed-on-bls12-377.params.js";
 import { tic, toc } from "./testing/tictoc.js";
 import { assert } from "./util.js";
-import { create } from "./module-twisted-edwards.js";
+import { create, startThreads, stopThreads } from "./module-twisted-edwards.js";
 
 const Curve = await create(curveParams);
 
-const n = 10;
+const n = 12;
 const N = 1 << n;
+
+await startThreads();
 
 // create random input points & scalars
 tic("random inputs");
@@ -21,16 +23,20 @@ let result = await Curve.Parallel.msm(scalars, points, N);
 let s0 = Curve.Curve.toBigint(result);
 toc();
 
-// convert points and scalars to bigints
-tic("inputs to bigints");
-let pointsBigint = points.map(Curve.Curve.toBigint);
-let scalarsBigint = scalars.map(Curve.Scalar.toBigint);
-toc();
+await stopThreads();
 
-// run bigint msm
-tic("bigint msm");
-let s1 = msmBigint(Curve.Bigint, scalarsBigint, pointsBigint);
-toc();
+if (n < 15) {
+  // convert points and scalars to bigints
+  tic("inputs to bigints");
+  let pointsBigint = points.map(Curve.Curve.toBigint);
+  let scalarsBigint = scalars.map(Curve.Scalar.toBigint);
+  toc();
 
-assert(Curve.Bigint.isEqual(s0, s1), "msm result mismatch");
-assert(Curve.Bigint.isZero(s1) === false, "msm result is zero");
+  // run bigint msm
+  tic("bigint msm");
+  let s1 = msmBigint(Curve.Bigint, scalarsBigint, pointsBigint);
+  toc();
+
+  assert(Curve.Bigint.isEqual(s0, s1), "msm result mismatch");
+  assert(Curve.Bigint.isZero(s1) === false, "msm result is zero");
+}
