5366effc1ba13455a84242bc8ad5c2949285ebf3
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2024-03-17 18:47:06 +0100

    rename

--- scripts/field-benchmarks/381.ts
+++ scripts/field-benchmarks/381.ts
@@ -1,4 +1,4 @@
 import { benchmark } from "./field-benchmark.js";
-import { BLS12_381 } from "../../src/concrete/bls12-381.js";
+import { BLS12381 } from "../../src/concrete/bls12-381.js";
 
-await benchmark(BLS12_381.Field);
+await benchmark(BLS12381.Field);
--- src/concrete/bls12-381.ts
+++ src/concrete/bls12-381.ts
@@ -2,6 +2,6 @@ import type * as _W from "wasmati";
 import { Weierstraß } from "../module-weierstrass.js";
 import { curveParams } from "./bls12-381.params.js";
 
-export { BLS12_381 };
+export { BLS12381 };
 
-let BLS12_381 = await Weierstraß.create(curveParams);
+let BLS12381 = await Weierstraß.create(curveParams);
