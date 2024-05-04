2c3b94efe97972446dd0865a13e2714cd9df801e
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2024-05-05 01:25:31 +0200

    support buffer inputs

--- src/parallel.ts
+++ src/parallel.ts
@@ -71,6 +71,45 @@ async function createWeierstraß(
     return Scalar.global.getPointer(size);
   }
 
+  // input bytes must be transfered to wasm memory before calling this function
+  function pointsFromBytes(pointPtr: number, pointInputPtr: number, n: number) {
+    let { size } = Affine;
+    let { fromPackedBytes, sizeField, toMontgomery, memoryBytes } = Field;
+
+    let [i, iend] = range(n);
+    let pi = pointPtr + i * size;
+    let bi = pointInputPtr + i * 96;
+
+    for (; i < iend; i++, pi += size, bi += 96) {
+      let x = pi;
+      let y = x + sizeField;
+      // set nonzero flag. (input format doesn't allow zero points, so always 1)
+      memoryBytes[pi + 2 * sizeField] = 1;
+
+      fromPackedBytes(x, bi);
+      fromPackedBytes(y, bi + 48);
+      toMontgomery(x);
+      toMontgomery(y);
+    }
+  }
+
+  // input bytes must be transfered to wasm memory before calling this function
+  function scalarsFromBytes(
+    scalarPtr: number,
+    scalarInputPtr: number,
+    n: number
+  ) {
+    let { fromPackedBytes, sizeField: size } = Scalar;
+
+    let [i, iend] = range(n);
+    let si = scalarPtr + i * size;
+    let bi = scalarInputPtr + i * 32;
+
+    for (; i < iend; i++, si += size, bi += 32) {
+      fromPackedBytes(si, bi);
+    }
+  }
+
   const Parallel = pool.register(`Weierstraß, ${label}`, {
     randomPointsFast,
     randomScalars,
@@ -78,6 +117,8 @@ async function createWeierstraß(
     msm,
     getPointer,
     getScalarPointer,
+    scalarsFromBytes,
+    pointsFromBytes,
   });
 
   const Bigint = { Projective: createBigintCurve(params) };
--- submission-bls377.ts
+++ submission-bls377.ts
@@ -6,18 +6,41 @@ export { compute_msm };
 await startThreads();
 let scratch = BLS12377.Field.local.getPointers(20);
 let nMax = 1 << 20;
+
+// pointers for data used by msm
 let pointPtr = await BLS12377.Parallel.getPointer(nMax * BLS12377.Affine.size);
 let scalarPtr = await BLS12377.Parallel.getScalarPointer(
   nMax * BLS12377.Scalar.sizeField
 );
 
+// pointers for input data
+let pointInputPtr = await BLS12377.Parallel.getPointer(nMax * 2 * 48);
+let scalarInputPtr = await BLS12377.Parallel.getScalarPointer(nMax * 32);
+
 async function compute_msm(
   inputPoints: BigIntPoint[] | U32ArrayPoint[] | Buffer,
   inputScalars: bigint[] | Uint32Array[] | Buffer
 ): Promise<{ x: bigint; y: bigint }> {
+  let n = 0;
+
   // transfer to wasm memory
-  await scalarsFromBigint(inputScalars as bigint[]);
-  await pointsFromBigint(inputPoints as BigIntPoint[]);
+  if (typeof inputScalars[0] === "bigint") {
+    n = inputScalars.length;
+    await scalarsFromBigint(inputScalars as bigint[]);
+  } else {
+    n = inputScalars.length / 32;
+    await scalarsFromBytes(inputScalars as Uint8Array);
+  }
+  if (
+    typeof inputPoints[0] === "object" &&
+    "x" in inputPoints[0] &&
+    typeof inputPoints[0].x === "bigint"
+  ) {
+    await pointsFromBigint(inputPoints as BigIntPoint[]);
+  } else {
+    await pointsFromBytes(inputPoints as Uint8Array);
+  }
+  using _ = BLS12377.Field.local.atCurrentOffset;
 
   // compute msm
   // IMPORTANT: we use `msmUnsafe`, which doesn't handle all edge cases of inputs, but is faster.
@@ -28,7 +51,7 @@ async function compute_msm(
     // let { result } = await BLS12377.Parallel.msmUnsafe(
     scalarPtr,
     pointPtr,
-    inputScalars.length
+    n
   );
 
   // return as affine bigint point
@@ -37,6 +60,26 @@ async function compute_msm(
   let resultBigint = BLS12377.Affine.toBigint(resultAffine);
 
   return resultBigint;
+}
+
+async function pointsFromBytes(inputPoints: Uint8Array) {
+  let n = inputPoints.length / (2 * 48);
+
+  // transfer input bytes to wasm memory
+  BLS12377.Field.memoryBytes.set(inputPoints, pointInputPtr);
+
+  // in parallel, convert input bytes to point representation
+  await BLS12377.Parallel.pointsFromBytes(pointPtr, pointInputPtr, n);
+}
+
+async function scalarsFromBytes(inputScalars: Uint8Array) {
+  let n = inputScalars.length / 32;
+
+  // transfer input bytes to wasm memory
+  BLS12377.Scalar.memoryBytes.set(inputScalars, scalarInputPtr);
+
+  // in parallel, convert input bytes to scalar representation
+  await BLS12377.Parallel.scalarsFromBytes(scalarPtr, scalarInputPtr, n);
 }
 
 export type BigIntPoint = { x: bigint; y: bigint; isZero: boolean };
