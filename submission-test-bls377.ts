5707f1a1251f4f5095cfd6f0b570d299636be343
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2024-05-05 01:20:53 +0200

    fix case of all same input points

--- submission-test-bls377.ts
+++ submission-test-bls377.ts
@@ -1,12 +1,18 @@
 import { compute_msm } from "./submission-bls377.js";
 import { curveParams } from "./src/concrete/bls12-377.params.js";
+import { BLS12377 } from "./src/concrete/bls12-377.js";
 // sanity check that the submission code can run and handles a basic test case
 
 let point = {
-  x: 22751760760637705504878160027698824622458942230958800913144762406971228535105703540544554687017331773376179053030n,
-  y: 174367514924512705429412616729947089042967561223700141222775688493826177468340587429479085947720121028088838723358n,
+  x: 111871295567327857271108656266735188604298176728428155068227918632083036401841336689521497731900230387779623820740n,
+  y: 76860045326390600098227152997486448974650822224305058012700629806287380625419427989664237630603922765089083164740n,
   isZero: false,
 };
+
+let Curve = BLS12377.Bigint.Projective;
+
+let p = Curve.fromAffine(point);
+console.log(Curve.isOnCurve(p), Curve.isInSubgroup(p));
 
 let scalars = [2n, curveParams.order - 1n];
 
@@ -17,4 +23,23 @@ let result = await compute_msm([point, point], scalars);
 if (result.x !== point.x || result.y !== point.y) {
   throw Error("failed");
 }
-console.log("ok");
+console.log("2 points ok");
+
+const n = 1000;
+let randomScalars = Array.from({ length: n }, () => Curve.Scalar.random());
+let randomPoints = BLS12377.Affine.randomPointsBigint(n);
+let samePoints = Array.from({ length: n }, () => point);
+let scalarSum = randomScalars.reduce(Curve.Scalar.add);
+
+// random points should work
+await compute_msm(randomPoints, randomScalars);
+console.log("random points ok");
+
+// msm should be the same as scaling by the sum of scalars
+let result2 = await compute_msm(samePoints, randomScalars);
+let result3 = await compute_msm([point], [scalarSum]);
+
+if (result2.x !== result3.x || result2.y !== result3.y) {
+  throw Error("failed");
+}
+console.log("same points ok");
