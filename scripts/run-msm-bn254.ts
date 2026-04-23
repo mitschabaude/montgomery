4d3dfe6b345f6bdd8bbaded9ca4fc1074d4736fa
Gregor <gregor.mitscha-baude@gmx.at>
2026-04-23 15:22:23 +0200

    add run-msm-bn254.ts benchmark runner

---
+++ scripts/run-msm-bn254.ts
@@ -0,0 +1,10 @@
+import { CurveParams } from "../src/index.ts";
+import { benchmarkMsm, runMsm } from "./msm-weierstrass.ts";
+
+console.log(process.argv.slice(2));
+let n = Number(process.argv[2] ?? 16);
+let nThreads = Number(process.argv[3] ?? 16);
+let doEvaluate = process.argv[4] === "--evaluate";
+
+if (doEvaluate) await benchmarkMsm(CurveParams.bn254, n, nThreads);
+else await runMsm(CurveParams.bn254, n, nThreads);
